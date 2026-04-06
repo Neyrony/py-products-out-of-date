91fabc663189fd6ba67fe27bb08dd57e7e4bb89b
Neyrony <sserebriy@gmail.com>
2026-04-06 09:48:11 +0300

    Implemented tests for py-products-out-of-date

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,68 @@
-# write your code here
+from datetime import date
+from unittest import mock
+
+
+import pytest
+
+
+from app.main import outdated_products
+
+
+@pytest.mark.parametrize("list_of_food, expected_output", [
+    ([
+    {
+        "name": "salmon",
+        "expiration_date": date(2026, 4, 10),
+    },
+    {
+        "name": "chicken",
+        "expiration_date": date(2026, 4, 8),
+    },
+    {
+        "name": "duck",
+        "expiration_date": date(2026, 4, 1),
+    }
+],
+    ["duck"]),
+    (
+        [],
+        []
+    ),
+    ([
+         {
+             "name": "turkey",
+             "expiration_date": date(2026, 4, 5),
+         },
+         {
+             "name": "cheese",
+             "expiration_date": date(2026, 4, 1),
+         },
+         {
+             "name": "chicken",
+             "expiration_date": date(2026, 3, 30),
+         }
+     ],
+        ["turkey", "cheese", "chicken"],
+     ),
+    (
+        [
+            {
+                "name": "beef",
+                "expiration_date": date(2026, 4, 10),
+            },
+            {
+                "name": "pork",
+                "expiration_date": date(2026, 4, 8),
+            },
+            {
+                "name": "tofu",
+                "expiration_date": date(2026, 12, 4),
+            }
+        ],
+        []
+    )
+])
+@mock.patch("app.main.datetime.date.today")
+def test_outdated_products(mocked_date_today, list_of_food, expected_output: list) -> None:
+    mocked_date_today.today.return_value = date(2026, 4, 6)
+    assert outdated_products(list_of_food) == expected_output
